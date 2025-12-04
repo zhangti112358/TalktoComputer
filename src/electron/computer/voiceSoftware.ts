09f4146b5303c3ddd3c07c27b9fb0190a8bee829
张锑 <43195706+zhangti112358@users.noreply.github.com>
2025-12-04 23:12:40 +0800

    更新 抽象类

--- src/electron/computer/voiceSoftware.ts
+++ src/electron/computer/voiceSoftware.ts
@@ -2,8 +2,11 @@
     语音软件实现
 */
 
+/*********************************************************************************************************************/
+// 抽象类
+
 // 说明
-export abstract class Document {
+export abstract class Information {
     public content: string = '';
 
     constructor(content: string) {
@@ -23,13 +26,15 @@ export enum BoolType {
 }
 
 // 参数说明
-export abstract class ParamDocument {
-    public document: Document;
+export abstract class IParam {
+    public name: string;
+    public information: Information;
     public type: DataType;
     public optional: boolean;
 
-    constructor(document: Document, type: DataType, optional: boolean = false) {
-        this.document = document;
+    constructor(name: string, information: Information, type: DataType, optional: boolean = false) {
+        this.name = name;
+        this.information = information;
         this.type = type;
         this.optional = optional;
     }
@@ -37,19 +42,52 @@ export abstract class ParamDocument {
 }
 
 // 函数
-export abstract class Function {
-    public document: Document;
-    public paramDocument: ParamDocument[];
-    public returnDocument: ParamDocument;
+export abstract class IFunction {
+    public name: string;
+    public information: Information;
+    public param: IParam[];
+    public return: IParam;
 
-    constructor(document: Document, paramDocument: ParamDocument[], returnDocument: ParamDocument) {
-        this.document = document;
-        this.paramDocument = paramDocument;
-        this.returnDocument = returnDocument;
+    constructor(name: string, information: Information, param: IParam[], returnParam: IParam) {
+        this.name = name;
+        this.information = information;
+        this.param = param;
+        this.return = returnParam;
     }
 
     abstract call(...args: any[]): any;
 }
+
+// 变量
+export abstract class IVariable {
+    public name: string;
+    public information: Information;
+    public type: DataType;
+
+    constructor(name: string, information: Information, type: DataType) {
+        this.name = name;
+        this.information = information;
+        this.type = type;
+    }
+}
+
+// 类
+export abstract class IClass {
+    public name: string;
+    public information: Information;
+    public variable_map: Map<string, IVariable>;
+    public function_map: Map<string, IFunction>;
+    constructor(name: string, information: Information, functions: IFunction[], variables: IVariable[]) {
+        this.name = name;
+        this.information = information;
+        this.function_map = new Map(functions.map(func => [func.name, func]));
+        this.variable_map = new Map(variables.map(variable => [variable.name, variable]));
+    }
+}
+
+/*********************************************************************************************************************/
+
+
 
 
 
